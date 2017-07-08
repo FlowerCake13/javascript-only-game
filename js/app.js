b474c0c564d29325f1086afc2e7a51839a23bea6
FlowerCake13 <lminami23@punahou.edu>
2017-07-08 11:38:52 -1000

    Preety Much Done

--- js/app.js
+++ js/app.js
@@ -1,68 +1,111 @@
 //console.log("dsajfosdfo")
 
-var canvas = document.createElement("canvas");
+function resizeWindow() {
+	var width = window.innerWidth-40;
+	var height = window.innerHeight-40;
+	var canvas = Documents.getElementsByTagName('canvas')[0];
+	canvas.width = width;
+	canvas.height = height;
+	canvas.style.width = width + 'px';
+	canvas.style.height = height + 'px';
+}
+ var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-canvas.style.width = canvas.width + "px";
-canvas.style.height = canvas.height + "px";
+canvas.style.width =canvas.width + 'px';
+canvas.style.height =canvas.height + 'px'; 
 document.body.appendChild(canvas);
 
-function resizeWindow() {
-	var width = window.innerHeight - 40;
-	var height = window.innerWidth - 40;
-	var canvas = document.getElementsByTagName('canvas')[0];
-	canvas.width = width;
-	canvas.height = height;
-	canvas.style.width = width + "px";
-	canvas.style.height = height + "px";
-}
 
 var bgReady = false;
-var bgImage = document.createElement("image");
-bgImage.onload = function (){
+var bgImage = document.createElement('img');
+bgImage.onload = function () {
 	bgReady = true;
 };
-bgImage.src = "https://raw.github.com/lostdecade/simple_canvas_game/master/images/background.png";
+bgImage.src = "sunset.jpg";
 
 var heroReady = false;
-var heroImage = document.createElement("image");
-heroImage.onload = function (){
+var heroImage = document.createElement('img');
+heroImage.onload = function () {
 	heroReady = true;
 };
-heroImage.src = "https://raw.github.com/lostdecade/simple_canvas_game/master/images/hero.png"
+heroImage.src = "cake.png";
 
 var monsterReady = false;
-var monsterImage = document.createElement("image");
-monsterImage.onload = function (){
+var monsterImage = document.createElement('img');
+monsterImage.onload = function () {
 	monsterReady = true;
 };
+monsterImage.src = "unicorn.png";
 
+	
+var hero = {
+	speed: 10
+	};
+var monster = {};
+var monstersCaught = 0;
+var keysDown = {};
+addEventListener("keydown",function (e) {
+	keysDown[e.keyCode] = true;
+}, false);
+addEventListener("keyup", function (e){
+	delete keysDown[e.keyCode];
+}, false);
+
+function reset(){
+	hero.x=canvas.width/2;
+	hero.y=canvas.height/2;
+	monster.x= 32 +(Math.random() * canvas.width-64); 
+	monster.y= 32 +(Math.random() * canvas.height-64);
+}
+function update(modifier){
+	if (38 in keysDown) {
+		hero.y -= hero.speed * modifier;
+	}
+	if (40 in keysDown) {
+		hero.y += hero.speed * modifier;
+	}
+
+	if (37 in keysDown) {
+		hero.x -= hero.speed * modifier;
+	}
+
+	if (39 in keysDown) {
+		hero.x += hero.speed * modifier;
+	}	
+ 	if (
+ 		hero.x<=(monster.x + 32)
+ 		&& monster.x <= (hero.x + 32)
+ 		&& hero.y <=(monster.y + 32)
+ 		&& monster.y <= (hero.y + 32)
+ 		){
+ 		++monstersCaught;
+ 			reset();
+ 	}
+ }
 function render(){
 	if (bgReady) {
 		ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
 	}
-	if (heroReady) {
+
+	if (heroReady){
 		ctx.drawImage(heroImage, hero.x, hero.y);
-	}
-	if (monsterReady) {
+}
+
+	if (monsterReady){
 		ctx.drawImage(monsterImage, monster.x, monster.y);
 	}
 }
+ 
 
-var then = Date.now();
-function main() {
-	var now = Date.now();
-	var delta = then - now;
+function main(){
+	var now = Date.now()
+	var delta = now - then;
+	update(delta/100);
 	render();
-	then = now;
+	then=now;
 }
-
-function reset() {
-	hero.x = canvas.width/2;
-	hero.y = canvas.height/2;
-	monster.x = Math.random(afdhadshf * (canvas.width-64))
-	monster.y = Math.random(afdhadshf * (canvas.height-64))
-}
-
-main(1);
+reset();
+var then = Date.now();
+setInterval(main,1);
